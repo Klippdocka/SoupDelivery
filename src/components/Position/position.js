2bd88130815e01434f80af2bb342c42a7c2e56a8
Elina Larsson <elina.larsson@student.kyh.se>
2020-09-17 09:46:04 +0200

    added google map auto complete

--- src/components/Position/position.js
+++ src/components/Position/position.js
@@ -11,9 +11,9 @@ justify-content:center;
 align-items:center;
 width:55%;
 height:6rem;
-background-color:white;
 opacity:0.8;
 border-radius:2.5rem;
+background-color:white;
 
 
 
@@ -31,10 +31,21 @@ align-items:center;
 text-align:center;
 
 
+
 `;
 
 const AddressContainer = styled.div`
-flex:5;
+flex:4;
+width:50rem;
+height:5rem;
+border-radius:2.5rem;
+position:relative;
+justify-content:start;
+align-items:center;
+@media screen and (max-width: ${theme.screenSize.small}){
+
+   width:70%;
+}
 
 
 `;
@@ -42,26 +53,52 @@ flex:5;
 
 const InputAddress = styled.input`
 width:90%;
-height:100%;
 font-size:2rem;
 border-radius:2.5rem;
 border:none;
+align-items:center;
+justify-content:center;
+margin-top:0.7rem;
+
+
 &::placeholder{
     color:#6c6c6c;
 
 }
 @media screen and (max-width: ${theme.screenSize.small}){
    height:4rem;
+   width:80%;
+}
+
+`;
+
+const DropDownStyle = styled.div`
+width:auto;
+opacity:1;
+
+@media screen and (max-width: ${theme.screenSize.small}){
+   width:80%;
 }
 
 `;
 
 
-const Position= (props) => {
+const InputWrapper = styled.div`
+
+    margin-top:0.4rem;
+   
+    
+    
+`;
+
+const Position = (props) => {
     
     const [address, setAddress] = useState("");
 
-    const handleSelect = async (value) => {};
+    const handleSelect = async (value) => {
+       console.log(value)
+
+    };
 
     
 
@@ -77,20 +114,37 @@ const Position= (props) => {
 
         <AddressContainer>
            
-            <PlacesAutocomplete value={address} onChange={setAddress} onSelect={handleSelect}>{({ getInputProps, suggestions, getSuggestionItemProps, loading }) =>(
-            <div>
-                    <InputAddress {...getInputProps({ placeholder: "Ange din adress" })}/>
+            <PlacesAutocomplete 
+            value={address} 
+            onChange={setAddress} 
+            onSelect={handleSelect}>
+
+            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+            <InputWrapper>
+                    <InputAddress {...getInputProps({ placeholder: "Ange din adress" })} />
 
                      <div>
                      {loading ? <div>...loading</div> : null }
 
-                        {suggestions.map((suggestions) => {
-                            return <div>{suggestions.descriptions}</div>
+                        {suggestions.map(suggestion => {
+                            const style = {
+                                backgroundColor: suggestion.active ? "#41b6e6" : "#fff"
+                            };
+                            
+
+                          
+                            return (
+                            <DropDownStyle  {...getSuggestionItemProps(suggestion, { style })}>
+                                {suggestion.description}
+
+                           
+                                </DropDownStyle>
+                                );
 
                         })}
 
                      </div>   
-                </div>
+                </InputWrapper>
                 
                 )}
 
